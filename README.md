90615e88a28deb60f09042bfa8665c1fa4acf29e
CyberParticle <>
2021-11-10 16:04:09 +0800

    init code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Media
 Pixie NFT contracts compatible with ERC721
+
+# Contract Address
+https://scan.chain.pixie.xyz/address/0xA335dD6f64EC53E0035786C020e1f187c790db42
+
+## Architecture
+
+This protocol is an extension of the ERC-721 NFT standard, intended to
+provide a metadata migrated schema for NFTs and future storage technique.
+This protocol refers to NFTs as `Media`.
+
+In the event that the URI that point to the data must be changed(for instance, to IPFS), this protocol offers the ability to update them.
+Recall that when minting tokens, sha256 hashes of the content are provided for integrity checks. As a result, anyone is able to
+check the integrity of the media if the URI change.
+
---
+++ contracts/interfaces/IMedia.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.8;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @title Interface for Pxbee Protocol's Media
+ */
+interface IMedia {
+    struct EIP712Signature {
+        uint256 deadline;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+    }
+
+    struct MediaData {
+        // A valid URI of the content represented by this token
+        string tokenURI;
+        // A SHA256 hash of the content pointed to by tokenURI
+        bytes32 contentHash;
+    }
+
+    event TokenURIUpdated(uint256 indexed _tokenId, address owner, string _uri);
+
+    /**
+     * @notice Mint new media for msg.sender.
+     */
+    //function mint(MediaData calldata data, IMarket.BidShares calldata bidShares) external;
+    function mint(uint256 tokenId, MediaData calldata data) external;
+
+    /**
+     * @notice Mint new media for creator.
+     */
+    //function mintForCreator(address creator, MediaData calldata data, IMarket.BidShares calldata bidShares) external;
+    function mintForCreator(address creator, uint256 tokenId, MediaData calldata data) external;
+
+    /**
+     * @notice EIP-712 mintWithSig method. Mints new media for a creator given a valid signature.
+     */
+    //function mintWithSig(address creator, MediaData calldata data, IMarket.BidShares calldata bidShares, EIP712Signature calldata sig) external;
+    function mintWithSig(address creator, uint256 tokenId, MediaData calldata data, EIP712Signature calldata sig) external;
+
+    /**
+     * @notice Revoke approval for a piece of media
+     */
+    function revokeApproval(uint256 tokenId) external;
+
+    /**
+     * @notice Update the token URI
+     */
+    function updateTokenURI(uint256 tokenId, string calldata tokenURI) external;
+
+    /**
+     * @notice EIP-712 permit method. Sets an approved spender given a valid signature.
+     */
+    function permit(address spender, uint256 tokenId, EIP712Signature calldata sig) external;
+}
